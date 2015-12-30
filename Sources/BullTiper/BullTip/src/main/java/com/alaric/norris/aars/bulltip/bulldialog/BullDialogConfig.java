3d2668f033c7861e4a061360f8c282f2c0ca664a
AlaricNorris <norris.sly@gmail.com>
2015-12-30 09:32:54 +0800

    bullog

---
+++ Sources/BullTiper/BullTip/src/main/java/com/alaric/norris/aars/bulltip/bulldialog/BullDialogConfig.java
@@ -0,0 +1,25 @@
+/**
+ *  BullDialog
+ *  com.alaric.norris.aars.bulltip.bulldialog
+ *  Function:       ${TODO} 
+ *  date            author
+ *  *****************************************************
+ *  2015/12/17         AlaricNorris
+ *  Copyright (c) 2015, TNT All Rights Reserved.
+ */
+package com.alaric.norris.aars.bulltip.bulldialog;
+
+/**
+ ClassName:      BullDialog
+ Function:       ${TODO}  ADD FUNCTION
+ Contact:        Norris.sly@gmail.com
+ @author         AlaricNorris 
+ @version        Ver 1.0   
+ @since          I used to be a programmer like you, then I took an arrow in the knee
+ ***************************************************************************************************
+ Modified By     AlaricNorris     2015/12/17    11:04
+ Modifications:  ${TODO}
+ ***************************************************************************************************
+ */
+public class BullDialogConfig {
+}
---
+++ Sources/BullTiper/BullTip/src/main/java/com/alaric/norris/aars/bulltip/bulltoast/BullToast.java
@@ -0,0 +1,25 @@
+/**
+ *  BullToast
+ *  com.alaric.norris.aars.bulltip.bulltoast
+ *  Function:       ${TODO} 
+ *  date            author
+ *  *****************************************************
+ *  2015/12/17         AlaricNorris
+ *  Copyright (c) 2015, TNT All Rights Reserved.
+ */
+package com.alaric.norris.aars.bulltip.bulltoast;
+
+/**
+ ClassName:      BullToast
+ Function:       ${TODO}  ADD FUNCTION
+ Contact:        Norris.sly@gmail.com
+ @author         AlaricNorris 
+ @version        Ver 1.0   
+ @since          I used to be a programmer like you, then I took an arrow in the knee
+ ***************************************************************************************************
+ Modified By     AlaricNorris     2015/12/17    11:02
+ Modifications:  ${TODO}
+ ***************************************************************************************************
+ */
+public class BullToast {
+}
